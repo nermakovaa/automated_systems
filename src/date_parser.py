582147baf89179ee738a256d961dcc6aca1ca864
dmsy4 <demsy2109@gmail.com>
2023-03-09 22:42:01 +0300

    Add first module files

---
+++ src/date_parser.py
@@ -0,0 +1,49 @@
+from datetime import datetime
+
+MONTH_MAP = {
+    "января": 1,
+    "февраля": 2,
+    "марта": 3,
+    "апреля": 4,
+    "мая": 5,
+    "июня": 6,
+    "июля": 7,
+    "августа": 8,
+    "сентября": 9,
+    "октября": 10,
+    "ноября": 11,
+    "декабря": 12,
+}
+
+
+def convert_to_datetime(date, time):
+    # format {day} {month}
+    if len(date.split()) == 2:
+        day, month_str = date.split()
+        day = int(day)
+        year = None
+    # format {day} {month} {year}
+    elif len(date.split()) == 3:
+        day, month_str, year = date.split()
+        day = int(day)
+        year = int(year)
+    else:
+        raise TypeError(f"Unexpected date: {date}")
+
+    assert 1 <= day <= 31, "Incorrect day parsed"
+    assert month_str in MONTH_MAP, "Incorrect month parsed"
+    if year:
+        assert year <= datetime.now().year, "Incorrect year parsed"
+    else:
+        year = datetime.now().year
+
+    month = MONTH_MAP[month_str]
+    hour, minute = map(int, time.split(":"))
+
+    assert 0 <= hour <= 23, "Incorrect hour"
+    assert 0 <= minute <= 59, "Incorrect minute"
+
+    datetime_str = f"{day}/{month:02d}/{year} {hour}:{minute:02d}"
+    datetime_object = datetime.strptime(datetime_str, "%d/%m/%Y %H:%M")
+
+    return datetime_object
---
+++ src/html_parser.py
@@ -0,0 +1,47 @@
+from collections import defaultdict
+from os.path import join
+from time import sleep
+
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup as BS
+
+from date_parser import convert_to_datetime
+
+URL = "https://knife.media/category/news/"
+
+
+def parse_url(first_page, last_page, save_table=True):
+    parsed_dict = defaultdict(list)
+
+    assert first_page > 0, "Page must be a positive integer"
+    assert last_page >= first_page, "Last page must be not less than first page"
+
+    for page in range(first_page, last_page + 1):
+
+        page_url = join(URL, f"page/{page}")
+        req = requests.get(page_url)
+        soup = BS(req.text, "lxml")
+
+        titles = soup.findAll("div", class_="widget-news__wrapper")
+
+        for title in titles:
+            article_title = title.find("a", class_="widget-news__content-link").text
+            article_link = title.find("a", class_="widget-news__content-link").get(
+                "href"
+            )
+            article_time = title.find("span", class_="meta__item").text
+            article_date = title.findAll("span", class_="meta__item")[1].text
+
+            article_datetime = convert_to_datetime(article_date, article_time)
+
+            parsed_dict["article_title"].append(article_title)
+            parsed_dict["article_link"].append(article_link)
+            parsed_dict["article_date"].append(article_datetime)
+            parsed_dict["page"].append(page)
+
+    table = pd.DataFrame(parsed_dict)
+    if save_table:
+        table.to_csv(f"pages {first_page} - {last_page}.csv")
+
+    return table
